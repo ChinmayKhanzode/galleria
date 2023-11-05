234f4a299ab809648e10451b9ee41cb6726de0a8
Chinmay Khanzode <70485580+ChinmayKhanzode@users.noreply.github.com>
2023-11-05 12:33:36 +0530

    added mongodb online url

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 
 console.log("insinde");
 
-const connection = mongoose.createConnection('mongodb://127.0.0.1:27017/galleria').on('open',()=>{
+const connection = mongoose.createConnection('mongodb+srv://chinmay:AaOtBYEM00XspUsv@cluster0.g3qee5d.mongodb.net/').on('open',()=>{
     console.log("Mongose connection succesfull");
 }).on('error',()=>{
     console.log("errorr has occured");
