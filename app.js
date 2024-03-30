8c367bc2892e9215627aaaeb0dda8c46f6e5ed51
Chinmay Khanzode <70485580+ChinmayKhanzode@users.noreply.github.com>
2024-03-30 23:21:35 +0530

    post chages

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const bodyParser = require("body-parser")
 
 const app = express();
 app.use(express.json())
-// app.use(bodyParser);
 
 app.use('/',userRouter);
 
--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,20 +1,20 @@
-
-
 const db = require('../config/db')
 
 exports.register = async(req,res,next)=>{
-    //console.log(res.body);
-    var imgurl = req.body.imgurl;
+    
+    var imgurl = req.query.imgurl;
+    console.log(req.query.imgurl);
+
 
     var myobj = { imgurl: imgurl };
     
     db.collection("images").insertOne(myobj, function(err, res) {  
-        console.log(res);
         if (err) throw err;
         console.log("1 record inserted");  
         });
 
-    res.json({requestBody: req.body});
+
+    res.json({requestBody: res.req.query.imgurl});
     console.log("all done");
     
 }
